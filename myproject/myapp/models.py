6c7bf3d5234f30887ac492014f4b089a184a358d
Artyom Batmanov <batmanov.2016@mail.ru>
2023-12-28 01:33:49 +0300

    final

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -11,7 +11,7 @@ class Item(models.Model):
     ]
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0, editable=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
 
 
